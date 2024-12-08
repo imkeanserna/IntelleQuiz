32b36e684e0abf86ea1ba4da35d86a7128a92951
imkurosaki <keandelaserna11@gmail.com>
2024-12-08 12:14:37 +0800

    fix: Fixed the authentication

--- backend/src/controllers/authentication.ts
+++ backend/src/controllers/authentication.ts
@@ -2,30 +2,33 @@ import { Response, Request } from 'express';
 import { generateToken } from '../lib/generateToken';
 
 const handleSocialSignIn = async (req: any, res: Response) => {
-   try {
-      console.log("handleSocialSignIn " + req.user);
-      const userId = req.user?.id;
+  try {
+    const userId = req.user?.id;
 
-      return res
-         .status(200)
-         .cookie('token', `Bearer ${generateToken({ userId: userId })}`)
-         .redirect(process.env.CLIENT_SSO_REDIRECT_URL || 'redirect_page');
-   } catch (e: any) {
-      console.log(e);
-   }
+    return res
+      .status(200)
+      .cookie('token', `Bearer ${generateToken({ userId: userId })}`, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'strict'
+      })
+      .redirect(`https://intellequiz.vercel.app/room?auth=success`);
+  } catch (e: any) {
+    console.log(e);
+  }
 }
 
 const getCurrentUser = async (req: Request, res: Response) => {
-   const user: any = req.user;
-   return res.status(200).json({
-      id: user.id,
-      username: user.username,
-      email: user.email,
-      image: user.image
-   });
+  const user: any = req.user;
+  return res.status(200).json({
+    id: user.id,
+    username: user.username,
+    email: user.email,
+    image: user.image
+  });
 }
 
 export const authController = {
-   handleSocialSignIn,
-   getCurrentUser
+  handleSocialSignIn,
+  getCurrentUser
 };
